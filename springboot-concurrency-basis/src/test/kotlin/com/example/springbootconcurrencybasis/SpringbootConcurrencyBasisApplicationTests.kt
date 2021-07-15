25ed2e1c30f98a0c4bba64e173e8ee3c819d51e6
pasudo (박성동) <>
2021-07-16 08:41:56 +0900

    [test] test code 수정

--- springboot-concurrency-basis/src/test/kotlin/com/example/springbootconcurrencybasis/SpringbootConcurrencyBasisApplicationTests.kt
+++ springboot-concurrency-basis/src/test/kotlin/com/example/springbootconcurrencybasis/SpringbootConcurrencyBasisApplicationTests.kt
@@ -6,7 +6,6 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
-@TestPropertySource(locations = ["classpath:application.yml"])
 class SpringbootConcurrencyBasisApplicationTests {
 
     @Test
--- springboot-jpa-basis/build.gradle.kts
+++ springboot-jpa-basis/build.gradle.kts
@@ -20,4 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // h2
+    testImplementation("com.h2database:h2")
 }

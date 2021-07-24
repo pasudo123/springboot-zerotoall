e090615d5dba1a43c2488efa5db19408ae4fe3c5
pasudo (박성동) <>
2021-07-24 15:50:03 +0900

    test code 작성 연습?

---
+++ springboot-testcode-basis/src/test/kotlin/com/example/springboottestcodebasis/exercise/Exercise03.kt
@@ -0,0 +1,39 @@
+package com.example.springboottestcodebasis.exercise
+
+import mu.KotlinLogging
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import kotlin.random.Random
+
+@Tag("banana")
+@DisplayName("@Tag 따라서 테스트코드를 수행시킬 수 있다.")
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class Exercise03 {
+
+    private val logger = KotlinLogging.logger {}
+
+    @Test
+    @DisplayName("bananaTest 01")
+    @Order(10)
+    fun bananaTest01() {
+        logger.info { "current test : ${Random.nextLong(1, 99999)}" }
+    }
+
+    @Test
+    @DisplayName("bananaTest 02")
+    @Order(20)
+    fun bananaTest02() {
+        logger.info { "current test : ${Random.nextLong(1, 99999)}" }
+    }
+
+    @Test
+    @DisplayName("appleTest 01")
+    @Order(30)
+    fun appleTest01() {
+        logger.info { "current test : ${Random.nextLong(1, 99999)}" }
+    }
+}

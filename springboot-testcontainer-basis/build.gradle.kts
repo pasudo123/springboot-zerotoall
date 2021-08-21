00147cb0b934d30419e7de51d9a55662bdf10392
pasudo (박성동) <>
2021-08-21 21:48:29 +0900

    feat : gradle version up 7.2

---
+++ springboot-testcontainer-basis/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm")
+}
+
+val junitJupiterVersion = "5.4.2"
+val kotestVersion = "4.6.1"
+val mockkVersion = "1.12.0"
+
+dependencies {
+
+    // jpa & mysql
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java")
+
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging:1.12.5")
+
+    // mock & kotest
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+
+    // testcontainer
+    testImplementation("org.testcontainers:testcontainers:1.16.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.16.0")
+}
---
+++ springboot-testcontainer-basis/src/main/kotlin/com/example/springboottestcontainerbasis/SpringbootTestcontainerBasisApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboottestcontainerbasis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootTestcontainerBasisApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootTestcontainerBasisApplication>(*args)
+}

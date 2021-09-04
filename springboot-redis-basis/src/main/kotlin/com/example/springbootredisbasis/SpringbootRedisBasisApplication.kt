34b99da26846af41cb723e0c5eec836c6982a4af
PARK SUNG DONG <oraedoa@gmail.com>
2021-09-04 14:13:06 +0900

    add : springboot-redis-basis

---
+++ springboot-redis-basis/src/main/kotlin/com/example/springbootredisbasis/SpringbootRedisBasisApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootredisbasis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootRedisBasisApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootRedisBasisApplication>(*args)
+}
---
+++ springboot-redis-basis/src/test/kotlin/com/example/springbootredisbasis/SpringbootRedisBasisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootredisbasis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootRedisBasisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

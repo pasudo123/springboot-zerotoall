7d645668ec695351fb158ba7d321936d5ef51885
PARK SUNG DONG <oraedoa@gmail.com>
2021-11-09 00:25:36 +0900

    test : fix

--- springboot-testcode-basis/src/test/kotlin/com/example/JpaAuditingTestConfiguration.kt
+++ springboot-testcode-basis/src/test/kotlin/com/example/JpaAuditingTestConfiguration.kt
@@ -6,10 +6,3 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 @Configuration
 @EnableJpaAuditing
 class JpaAuditingBaseConfiguration
-
-/**
- * fakeAuditingDateTimeProvider 를 별도로 정의해서 사용할 수 있도록 한다.
- */
-@Configuration
-@EnableJpaAuditing(dateTimeProviderRef = "fakeAuditingDateTimeProvider")
-class JpaAuditingFakeConfiguration

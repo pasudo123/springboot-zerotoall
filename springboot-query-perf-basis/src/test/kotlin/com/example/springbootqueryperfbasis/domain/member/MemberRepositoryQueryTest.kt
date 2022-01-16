af70596ee99da27f36d37673ce1bbb4cf5ba1a08
PARK SUNG DONG <oraedoa@gmail.com>
2022-01-16 20:53:31 +0900

    feat : test

--- springboot-query-perf-basis/src/test/kotlin/com/example/springbootqueryperfbasis/domain/member/MemberRepositoryQueryTest.kt
+++ springboot-query-perf-basis/src/test/kotlin/com/example/springbootqueryperfbasis/domain/member/MemberRepositoryQueryTest.kt
@@ -2,8 +2,7 @@ package com.example.springbootqueryperfbasis.domain.member
 
 import com.example.springbootqueryperfbasis.RepositorySupport
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.annotation.Commit
@@ -19,30 +18,31 @@ class MemberRepositoryQueryTest(
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @TestFactory
+    @DisplayName("jpa saveAll() 을 수행한다.")
+    @Test
     @Commit
-    fun saveQueryPerfTest(): List<DynamicTest> {
-        val memberAGroup = mutableListOf<Member>()
-        val memberBGroup = mutableListOf<Member>()
+    fun saveQueryPerfTest() {
+        val memberGroup = mutableListOf<Member>()
 
-        repeat(1000000) {
-            memberAGroup.add(MemberFixture.aEntity())
-            memberBGroup.add(MemberFixture.aEntity())
+        repeat(100000) {
+            memberGroup.add(MemberFixture.aEntity())
         }
 
-        return listOf(
-            DynamicTest.dynamicTest("jpa saveAll() : insert line by line 을 수행한다.") {
-                val elapsed = measureTimeMillis {
-                    memberRepository.saveAll(memberAGroup)
-                    entityManager.flush()
-                    entityManager.clear()
-                }
+        val elapsed = measureTimeMillis {
+            memberRepository.saveAll(memberGroup)
+            entityManager.flush()
+            entityManager.clear()
+        }
 
-                log.info("elapsed : ${(elapsed / 1000.0)}sec")
-            },
-            DynamicTest.dynamicTest("insert multi line 을 수행한다.") {
+        log.info("elapsed : ${(elapsed / 1000.0)}sec")
 
-            }
-        )
+        /**
+         * 10만건 넣는것 기준
+         *  hibernate.jdbc.batch_size: 5   로 했을 때 소요시간 :: 27.024sec
+         *  hibernate.jdbc.batch_size: 50  로 했을 때 소요시간 :: 31.009sec
+         *  hibernate.jdbc.batch_size: 100 로 했을 때 소요시간 :: 34.017sec
+         *  hibernate.jdbc.batch_size: 200 로 했을 때 소요시간 :: 26.737sec (rewriteBatchedStatements=true 옵션이 없는 경우)
+         *  hibernate.jdbc.batch_size: 200 로 했을 때 소요시간 :: 36.737sec (rewriteBatchedStatements=true 옵션이 있는 경우)
+         */
     }
 }

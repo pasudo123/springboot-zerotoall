eb9d98c01d8274d8dadfb8fc62acb9192829a406
PARK SUNG DONG <oraedoa@gmail.com>
2022-01-16 16:33:51 +0900

    project

---
+++ springboot-query-perf-basis/src/main/kotlin/com/example/springbootqueryperfbasis/SpringbootQueryPerfBasisApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootqueryperfbasis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootQueryPerfBasisApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootQueryPerfBasisApplication>(*args)
+}
---
+++ springboot-query-perf-basis/src/test/kotlin/com/example/springbootqueryperfbasis/SpringbootQueryPerfBasisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootqueryperfbasis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootQueryPerfBasisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

203119a9f08e8fbe14e3c176679241a603871848
PARK SUNG DONG <pasudo123@naver.com>
2022-05-29 22:37:34 +0900

    refactor

---
+++ springboot-hadoop-basis/src/main/kotlin/com/example/springboothadoopbasis/SpringbootHadoopBasisApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboothadoopbasis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootHadoopBasisApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootHadoopBasisApplication>(*args)
+}

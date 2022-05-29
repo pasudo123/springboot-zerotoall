f27b24298c81c540569e9c61128408a92bbaa29c
PARK SUNG DONG <pasudo123@naver.com>
2022-05-29 21:31:01 +0900

    refactor

---
+++ pure-kotlin/src/test/kotlin/leetcode/2287.kt
@@ -0,0 +1,44 @@
+package leetcode
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import kotlin.math.min
+
+class `2287` {
+
+    @Test
+    fun leetcodeTest() {
+        rearrangeCharacters("abbaccaddaeea", "aaaaa") shouldBe 1
+        rearrangeCharacters("abcba", "abc") shouldBe 1
+        rearrangeCharacters("ilovecodingonleetcode", "code") shouldBe 2
+    }
+}
+
+fun rearrangeCharacters(s: String, target: String): Int {
+
+    val toBeCountGroup = mutableMapOf<Char, Int>()
+    val countGroup = mutableMapOf<Char, Int>()
+
+    target.forEach { ch ->
+        if (countGroup.containsKey(ch).not()) {
+            countGroup[ch] = 1
+        } else {
+            countGroup[ch] = countGroup[ch]!! + 1
+        }
+
+        toBeCountGroup[ch] = 0
+    }
+
+    s.forEach { ch ->
+        if (countGroup.containsKey(ch)) {
+            toBeCountGroup[ch] = toBeCountGroup[ch]!! + 1
+        }
+    }
+
+    var result = Integer.MAX_VALUE
+    toBeCountGroup.keys.forEach { key ->
+        result = min(result, toBeCountGroup[key]!!.div(countGroup[key]!!))
+    }
+
+    return result
+}

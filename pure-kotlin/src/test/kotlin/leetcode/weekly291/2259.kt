8d36e8452893f4ef0864b09657ff0218ca5e7517
PARK SUNG DONG <pasudo123@naver.com>
2022-06-06 12:52:02 +0900

    leetcode

---
+++ pure-kotlin/src/test/kotlin/leetcode/weekly291/2259.kt
@@ -0,0 +1,41 @@
+package leetcode.weekly291
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class `2259` {
+    @Test
+    fun leetcodeTest() {
+        removeDigit("123", '3') shouldBe "12"
+        removeDigit("1231", '1') shouldBe "231"
+        removeDigit("551", '5') shouldBe "51"
+        removeDigit("133235", '3') shouldBe "13325"
+    }
+}
+
+fun removeDigit(number: String, digit: Char): String {
+    var result = "0"
+    for (index in number.indices) {
+        val findIndex = number.indexOf(digit, index)
+        if (findIndex == -1) {
+            break
+        }
+
+        val currentValue = (number.substring(0, findIndex) + number.substring(findIndex + 1, number.length))
+
+        if (currentValue.count() > result.count()) {
+            result = currentValue
+        } else {
+            for (subIndex in result.indices) {
+                if (currentValue[subIndex] > result[subIndex]) {
+                    result = currentValue
+                    break
+                } else if (currentValue[subIndex] < result[subIndex]) {
+                    break
+                }
+            }
+        }
+    }
+
+    return result
+}

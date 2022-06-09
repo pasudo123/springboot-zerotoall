2c8944b0c759e2132c600b0dab706ed2317931f0
PARK SUNG DONG <pasudo123@naver.com>
2022-06-09 23:35:27 +0900

    leetcode failed

--- pure-kotlin/src/test/kotlin/leetcode/weekly290/2249.kt
+++ pure-kotlin/src/test/kotlin/leetcode/weekly290/2249.kt
@@ -9,7 +9,7 @@ class `2249` {
     @Test
     fun leetcodeTest() {
 
-//        countLatticePoints(arrayOf(intArrayOf(2, 2, 1))) shouldBe 5
+        countLatticePoints(arrayOf(intArrayOf(2, 2, 1))) shouldBe 5
         countLatticePoints(arrayOf(intArrayOf(2, 2, 2), intArrayOf(3, 4, 1))) shouldBe 16
     }
 }
@@ -23,19 +23,32 @@ fun countLatticePoints(circles: Array<IntArray>): Int {
         val r = circle[2]
 
         group.add("$y&$x")
-        for (size in 1..r) {
-            group.add("${x}&${y-size}")
-            group.add("${x}&${y+size}")
-            group.add("${x-size}&${y}")
-            group.add("${x+size}&${y}")
 
-            if (size < r) {
-                group.add("${x - size}&${y - size}")
-                group.add("${x + size}&${y - size}")
-                group.add("${x - size}&${y + size}")
-                group.add("${x + size}&${y + size}")
+        if (r == 1) {
+            group.add("${x}&${y - 1}")
+            group.add("${x}&${y + 1}")
+            group.add("${x - 1}&${y}")
+            group.add("${x + 1}&${y}")
+            return@forEach
+        }
+
+        for (size in 1 until r) {
+            val pairs = listOf(
+                Pair(size, 0), Pair(-size, 0),
+                Pair(0, size), Pair(0, -size),
+                Pair(size, size), Pair(size, -size),
+                Pair(-size, size), Pair(-size, -size)
+            )
+
+            pairs.forEach { pair ->
+                group.add("${y + pair.first}&${x + pair.second}")
             }
         }
+
+        group.add("${y - r}&$x")
+        group.add("${y + r}&$x")
+        group.add("${y}&${x-r}")
+        group.add("${y}&${x+r}")
     }
 
     return group.size

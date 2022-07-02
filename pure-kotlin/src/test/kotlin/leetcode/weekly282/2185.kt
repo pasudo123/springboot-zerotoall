4b3003a104e2a89fa084d66c9451cf4d871a9cd2
PARK SUNG DONG <pasudo123@naver.com>
2022-07-02 17:58:09 +0900

    init

--- pure-kotlin/src/test/kotlin/leetcode/weekly282/2185.kt
+++ pure-kotlin/src/test/kotlin/leetcode/weekly282/2185.kt
@@ -1,4 +1,25 @@
 package leetcode.weekly282
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
 class `2185` {
+
+    @Test
+    fun leetcodeTest() {
+        prefixCount(arrayOf("pay","attention","practice","attend"), "at") shouldBe 2
+        prefixCount(arrayOf("leetcode","win","loops","success"), "code") shouldBe  0
+    }
+}
+
+fun prefixCount(words: Array<String>, pref: String): Int {
+    var count = 0
+
+    words.forEach { word ->
+        if (word.startsWith(pref)) {
+            count++
+        }
+    }
+
+    return count
 }
--- pure-kotlin/src/test/kotlin/leetcode/weekly283/2195.kt
+++ pure-kotlin/src/test/kotlin/leetcode/weekly283/2195.kt
@@ -1,4 +1,21 @@
 package leetcode.weekly283
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
 class `2195` {
+
+    @Test
+    fun leetcodeTest() {
+
+        minimalKSum(intArrayOf(1,4,25,10,25), 2) shouldBe 5
+        minimalKSum(intArrayOf(5, 6), 6) shouldBe 25
+    }
+}
+
+fun minimalKSum(nums: IntArray, k: Int): Long {
+    // 100000000
+    var sum = 0L
+
+    return sum
 }

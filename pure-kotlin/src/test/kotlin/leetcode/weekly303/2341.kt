4ee1b748a7a3545238dc7bd9c8dcc7507224c02e
PARK SUNG DONG <oraedoa@gmail.com>
2022-07-21 22:02:22 +0900

    leetcode

---
+++ pure-kotlin/src/test/kotlin/leetcode/weekly303/2341.kt
@@ -0,0 +1,39 @@
+package leetcode.weekly303
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class `2341` {
+
+    @Test
+    @DisplayName("테스트코드")
+    fun leetcodeTest() {
+
+        numberOfPairs(intArrayOf(1,3,2,1,3,2,2)) shouldBe intArrayOf(3,1)
+        numberOfPairs(intArrayOf(1,1)) shouldBe intArrayOf(1,0)
+        numberOfPairs(intArrayOf(0)) shouldBe intArrayOf(0,1)
+    }
+}
+
+fun numberOfPairs(nums: IntArray): IntArray {
+    val array = Array(101) { 0 }
+
+    nums.forEach { num ->
+        array[num]++
+    }
+
+    var pairCount = 0
+    var size = 0
+
+    array.forEach { element ->
+        if (element == 0) {
+            return@forEach
+        }
+
+        pairCount += element / 2
+        size += element % 2
+    }
+
+    return intArrayOf(pairCount, size)
+}

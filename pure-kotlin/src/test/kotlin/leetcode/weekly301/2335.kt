c01de614f48e2ec3eb41bf0c06f50f4d6442022b
PARK SUNG DONG <oraedoa@gmail.com>
2022-07-23 01:57:43 +0900

    leetcode

---
+++ pure-kotlin/src/test/kotlin/leetcode/weekly301/2335.kt
@@ -0,0 +1,39 @@
+package leetcode.weekly301
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class `2335` {
+
+    @Test
+    @DisplayName("테스트코드")
+    fun leetcodeTest() {
+//        fillCups(intArrayOf(1, 4, 2)) shouldBe 4
+//        fillCups(intArrayOf(5, 4, 4)) shouldBe 7
+        fillCups(intArrayOf(5, 0, 0)) shouldBe 5
+    }
+}
+
+fun fillCups(amount: IntArray): Int {
+
+    val sum = amount.sum()
+    if (sum == 0) {
+        return 0
+    }
+
+    amount.sortDescending()
+
+    if (amount[0] >= 1 && amount[1] >= 1) {
+        amount[0] -= 1
+        amount[1] -= 1
+        return 1 + fillCups(amount)
+    }
+
+    if (amount[0] >= 1) {
+        amount[0] -= 1
+        return 1 + fillCups(amount)
+    }
+
+    return 0
+}

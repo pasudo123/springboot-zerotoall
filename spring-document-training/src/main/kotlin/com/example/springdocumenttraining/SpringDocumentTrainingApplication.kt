a45acd21f0c9dba7a40375b1d963226c3c99cbdd
PARK SUNG DONG <oraedoa@gmail.com>
2023-10-28 08:07:52 +0900

    study

---
+++ spring-document-training/src/main/kotlin/com/example/springdocumenttraining/SpringDocumentTrainingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdocumenttraining
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDocumentTrainingApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDocumentTrainingApplication>(*args)
+}
---
+++ spring-document-training/src/test/kotlin/com/example/springdocumenttraining/SpringDocumentTrainingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdocumenttraining
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDocumentTrainingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

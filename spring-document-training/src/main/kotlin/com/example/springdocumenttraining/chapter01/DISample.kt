0f0fd26e7922fd3b33b706814332cb05115ac08a
PARK SUNG DONG <oraedoa@gmail.com>
2023-11-04 12:36:52 +0900

    study

---
+++ spring-document-training/src/main/kotlin/com/example/springdocumenttraining/chapter01/DISample.kt
@@ -0,0 +1,29 @@
+package com.example.springdocumenttraining.chapter01
+
+class DISample
+
+fun main() {
+    val myController = MyController()
+    myController.index()
+}
+
+interface MyService {
+    fun task()
+}
+
+class MyCoffeeService : MyService {
+    override fun task() {}
+}
+
+class MySnackService : MyService  {
+    override fun task() {}
+}
+
+class MyController{
+
+    private val myService: MyService = MyCoffeeService()
+
+    fun index() {
+        myService.task()
+    }
+}

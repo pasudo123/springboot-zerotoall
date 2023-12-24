064f0ee7afe625e8aab1df014059fb958ce99fba
PARK SUNG DONG <oraedoa@gmail.com>
2023-12-24 11:02:59 +0900

    루트 프로젝트 이름 변경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "springboot-kotlin-zerotoall"
+rootProject.name = "springboot-zerotoall"
 include("springboot-gql-basis")
 include("springboot-pageable-basis")
 include("springboot-testcontainer-basis")

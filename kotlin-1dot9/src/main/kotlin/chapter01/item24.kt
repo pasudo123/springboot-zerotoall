a5cd961245667f6660ab5943e5c3c22ac677d970
PARK SUNG DONG <oraedoa@gmail.com>
2024-03-02 15:51:04 +0900

    study

---
+++ kotlin-1dot9/src/main/kotlin/chapter01/item24.kt
@@ -0,0 +1,38 @@
+package chapter01
+
+class item24
+
+data class BoxOut<out T>(
+    val value: T
+)
+
+data class BoxIn<in T : Any>(
+    val value: Long
+)
+
+fun main() {
+
+    val names = listOf("Hong", "Park")
+
+    val nameLengths: List<Long> = runCatching {
+        names.map { it.length }
+        throw RuntimeException("강제에러")
+    }.onSuccess {
+        println("success")
+    }.onFailure { exception ->
+        println("fail. message=${exception.message}")
+    }.getOrElse {
+        println("getOrElse")
+        listOf(1, 2, 3)
+    }
+
+    println(nameLengths)
+
+    val boxOut1: BoxOut<Int> = BoxOut(5555)
+    val boxOut2: BoxOut<Any> = boxOut1
+    println(boxOut2.value)
+
+    val boxIn1: BoxIn<Any> = BoxIn(4444)
+    val boxIn2: BoxIn<Long> = boxIn1
+    println(boxIn2.value)
+}

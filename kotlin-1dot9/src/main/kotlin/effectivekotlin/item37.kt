ed31c499442a4642f4d50a3c316e377606c1cc4b
PARK SUNG DONG <oraedoa@gmail.com>
2024-03-24 17:16:18 +0900

    docs

--- kotlin-1dot9/src/main/kotlin/effectivekotlin/item37.kt
+++ kotlin-1dot9/src/main/kotlin/effectivekotlin/item37.kt
@@ -1,4 +1,34 @@
 package effectivekotlin
 
-class item73 {
+class item37
+
+data class Player(
+    val name: String,
+    val age: Int,
+    val gender: Gender
+) {
+    enum class Gender {
+        M, W
+    }
+}
+
+fun main() {
+    val player1 = Player("park", 10, Player.Gender.M)
+    val player2 = Player("park", 10, Player.Gender.M)
+    val player1Reference = player1
+
+    // 구조적 동등성 : ==
+    println("구조적 동등성")
+    println(player1 == player2) // true
+    println(player1 == player1Reference) // true
+
+    // 레퍼런스적 동등성 : ===
+    println("레퍼런스적 동등성")
+    println(player1 === player2) // false
+    println(player1 === player1Reference) // true
+
+    val player3 = player1.copy(name = "kim")
+    println("테스트")
+    println(player1 == player3) // false
+    println(player1 === player3) // false
 }
--- kotlin-1dot9/src/main/kotlin/effectivekotlin/item40.kt
+++ kotlin-1dot9/src/main/kotlin/effectivekotlin/item40.kt
@@ -1,4 +1,17 @@
 package effectivekotlin
 
-class item40 {
+import java.net.URI
+import java.net.URL
+
+class item40
+
+fun main() {
+    val wiki1 = URL("https://en.wikipedia.org/")
+    val wiki2 = URL("https://wikipedia.org/")
+
+    val uri1 = URI("https://en.wikipedia.org/")
+    val uri2 = URI("https://wikipedia.org/")
+
+    println(wiki1 == wiki2)
+    println(uri1 == uri2)
 }

d02960e9e0db05c8df35296bd0e9ff9783aa62c4
PARK SUNG DONG <oraedoa@gmail.com>
2024-05-08 00:47:14 +0900

    study : jackson 이슈 처리

---
+++ springboot-jpa-basis/src/main/kotlin/com/example/springbootjpabasis/config/CustomObjectMapperConfiguration.kt
@@ -0,0 +1,28 @@
+package com.example.springbootjpabasis.config
+
+import com.example.springbootjpabasis.config.CustomObjectMapperConfiguration.Companion.mapper
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CustomObjectMapperConfiguration {
+
+    companion object {
+        val mapper = ObjectMapper()
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .registerModule(JavaTimeModule())
+            .registerKotlinModule()
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return mapper
+    }
+}
+
+inline fun <reified T : Any> T.toJson(): String = mapper.writeValueAsString(this)
+inline fun <reified T : Any> String.toObject(): T = mapper.readValue(this, T::class.java)
---
+++ springboot-jpa-basis/src/test/kotlin/com/example/springbootjpabasis/jackson/JacksonTestWithJava.java
@@ -0,0 +1,10 @@
+package com.example.springbootjpabasis.jackson;
+
+public class JacksonTestWithJava {
+
+    public void sample() {
+        Dummy dummy = new Dummy("as", "as");
+        System.out.println(dummy.aName);
+        System.out.println(dummy.bName);
+    }
+}

1bc3fa0b0898a385b98b0a7830306751bbe34c69
PARK SUNG DONG <oraedoa@gmail.com>
2024-06-13 00:36:20 +0900

    study : coroutine-scope

---
+++ kotlin-1dot9/src/main/kotlin/coroutine/KotlinCoroutine06.kt
@@ -0,0 +1,32 @@
+package coroutine
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class KotlinCoroutine06
+
+fun main() {
+    runBlocking {
+        coroutineScopeFunc()
+        runBlockingFunc()
+    }
+}
+
+// 코루틴 빌더를 사용하기 위해서, coroutineScope 로 wrapping
+suspend fun coroutineScopeFunc() = coroutineScope {
+    launch { println("hello") }
+}
+
+// 코루틴 빌더를 사용하기 위해서, runBlocking 로 wrapping
+suspend fun runBlockingFunc() = runBlocking {
+    launch { println("world") }
+}
+
+/**
+ * 코루틴 빌더를 사용하기 위해선, 코루틴 스코프 혹은 runBlocking 이 필요하다.
+ * 없다면 컴파일 에러가 발생한다. (아래 코드는 컴파일 에러가 발생하는 코드이다.)
+ */
+//suspend fun basicFunc() {
+//    launch {  } -> 컴파일 에러 발생.
+//}

39bae72d190f9ac7abf45c64f7eb27839d1eb972
PARK SUNG DONG <oraedoa@gmail.com>
2024-08-15 17:03:56 +0900

    코루틴 스코프 함수.

---
+++ kotlin-1dot9/src/main/kotlin/coroutine/ChatGPTLocalScope.kt
@@ -0,0 +1,31 @@
+package coroutine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class ChatGPTLocalScope
+
+/**
+ * 아래코드는 권장되는 방식이 아니다.
+ * 차라리 하려면 구조화된 코루틴 형식으로 바꿔야한다.
+ */
+fun main() = runBlocking {
+    val localScope = CoroutineScope(Dispatchers.IO)
+
+    localScope.launch {
+        delay(1000)
+        println("hello world")
+    }
+
+    /**
+     * join() 을 통해서 기다린다. 메인스레드가 block 된다.
+     * join() 이 없다면 메인스레드가 block 되지 않는다.
+     */
+    localScope.coroutineContext[Job]?.join()
+
+    println("done")
+}
---
+++ kotlin-1dot9/src/main/kotlin/coroutine/KotlinCoroutine11.kt
@@ -0,0 +1,51 @@
+package coroutine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+class KotlinCoroutine11
+
+data class Details(
+    val name: String,
+    val followers: Int,
+)
+
+data class Tweet(
+    val text: String
+)
+
+fun getFollowersNumber(): Int = throw RuntimeException("팔로워 수 획득 실패")
+
+suspend fun getUserName(): String {
+    delay(500)
+    return "PARK"
+}
+
+suspend fun getTweets(): List<Tweet> {
+    delay(300)
+    return listOf(Tweet("Hello"), Tweet("World"))
+}
+
+/**
+ * 아래 내용을 coroutineScope, 코루틴 스코프 함수로 변경하면 예외에도 유연하게 대처할 수 있다.
+ */
+suspend fun CoroutineScope.getUserDetails(): Details {
+    val userName = async(Dispatchers.IO) { getUserName() }
+    val followersNumber = async(Dispatchers.IO) { getFollowersNumber() }
+    return Details(userName.await(), followersNumber.await())
+}
+
+fun main() = runBlocking {
+    val details = try {
+        getUserDetails()
+    } catch (exception: Exception) {
+        null
+    }
+
+    val tweets = async { getTweets() }
+    println("user=$details")
+    println("tweets=${tweets.await()}")
+}
